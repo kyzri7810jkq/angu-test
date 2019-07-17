0a6b8268d840b27c66bdd2b71e1e0cf8104881e0
Sam <sam@gmail.com>
2019-07-17 22:14:03 +0800

    Added routeActiveLink on home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostsComponent } from './posts/posts.component';
 import { HomeComponent } from './home/home.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 const routes: Routes = [
-	{ path: 'posts', component: PostsComponent }, 
-  { path: '', component: HomeComponent }
+  { path: 'posts', component: PostsComponent }, 
+  { path: 'employee', component: EmployeeComponent }, 
+  { path: '', component: HomeComponent, data: { title: 'Welcome Home' } }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false } )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { HomeComponent } from './home/home.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
     PostsComponent,
-    HomeComponent
+    HomeComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
