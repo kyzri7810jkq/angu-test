b8621894545ff51ba7a7c04afe5083d1c702d9ae
Sam <samuel.amador@freemight.com>
2019-07-17 19:53:29 +0800

    Fixed student URI http://localhost:4200/students/list

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -4,12 +4,20 @@ import { StudentComponent } from './student/student.component';
 import { StudentListComponent } from './student-list/student-list.component';
 import { StudentListItemComponent } from './student-list-item/student-list-item.component';
 
+import { RouterModule, Routes } from '@angular/router'
 
+const routes: Routes = [
+  { path: 'list', component: StudentListComponent },
+  { path: '', component: StudentListComponent }
+]
 
 @NgModule({
   declarations: [StudentComponent, StudentListComponent, StudentListItemComponent],
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class StudentModule { }
