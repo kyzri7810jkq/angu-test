c1479af0c84c446998036d02219f66a7fab96388
Sam <sam@gmail.com>
2019-07-18 22:05:05 +0800

    Added parts header footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { HeaderComponent } from './parts/header/header.component';
+import { FooterComponent } from './parts/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { EmployeeComponent } from './employee/employee.component';
     routingComponents,
     PostsComponent,
     HomeComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
